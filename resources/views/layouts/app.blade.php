b05131ee68ef9139916ab2a6978fe9a7a0c94c07
Abdul Ghofar <ofaasd@gmail.com>
2026-05-11 14:57:59 +0700

    update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,10 @@
             {{ $slot }}
 
             <footer class="main-footer">
-                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+                <strong>Copyright &copy; 2026 <a href="https://rmediasolusindo.com">Rahman Media Solusindo</a>.</strong>
                 All rights reserved.
                 <div class="float-right d-none d-sm-inline-block">
-                  <b>Version</b> 3.2.0
+                  <b>Version</b> 1.0.0 (beta)
                 </div>
             </footer>
         </div>
