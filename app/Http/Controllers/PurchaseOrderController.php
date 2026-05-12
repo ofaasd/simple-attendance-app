d204473c5b9a4b19b6e3c6ae22a1b0cd117ccb8e
Abdul Ghofar <ofaasd@gmail.com>
2026-05-12 19:03:50 +0700

    update purchase order

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -810,10 +810,6 @@ class PurchaseOrderController extends Controller
                     'keterangan' => 'di generate oleh sistem setelah PO disetujui kepala SPPG',
                 ]);
 
-                //tambahkan pengurangan saldo 
-                $sppg = Sppg::find($purchaseOrder->sppg_id);
-                $sppg->saldo -= $totalRealisasi;
-                $sppg->save();
             }
         });
 
